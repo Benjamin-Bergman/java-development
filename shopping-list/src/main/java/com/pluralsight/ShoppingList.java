898d6d855765642c5cd97dd4c94cc346a2329e8d
Benjamin Bergman <git@gdane.net>
2024-04-10 14:08:59 -0400

    Add shopping list

---
+++ shopping-list/src/main/java/com/pluralsight/ShoppingList.java
@@ -0,0 +1,28 @@
+package com.pluralsight;
+
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class ShoppingList {
+    public static void main(String[] args) {
+        final String[] items = new String[]
+                {
+                        "Eggs", "Milk", "Bread", "Chicken", "Sauce",
+                        "Soup", "Vegetables", "Apples", "Celery", "Juice"
+                };
+//        final String[] items = IntStream.range(0, 10).mapToObj(x -> items_i).flatMap(Arrays::stream).toArray(String[]::new);
+        final String format = "%" +
+                digitsIn(items.length) +
+                "d. %s";
+        System.out.println("My shopping list (" + items.length + " items):");
+        System.out.println(
+                IntStream.range(0, items.length)
+                        .mapToObj(i -> String.format(format, i + 1, items[i]))
+                        .collect(Collectors.joining(System.lineSeparator())));
+//        System.out.println("1. Eggs\n2. Milk\n3. Bread\n4. Chicken\n5. Sauce\n6. Soup\n7. Vegetables\n8. Apples\n9. Celery\n10.Juice");
+    }
+
+    private static int digitsIn(int x) {
+        return (int) Math.floor(Math.log10(x) + 1);
+    }
+}
