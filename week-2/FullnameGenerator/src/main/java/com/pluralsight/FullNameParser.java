a0fecf5c7481e82d9fe4c51cb458590d52768322
Benjamin-Bergman <git@gdane.net>
2024-04-15 15:28:22 -0400

    Close resources

--- week-2/FullnameGenerator/src/main/java/com/pluralsight/FullNameParser.java
+++ week-2/FullnameGenerator/src/main/java/com/pluralsight/FullNameParser.java
@@ -4,21 +4,23 @@ package com.pluralsight;
 
 import java.util.*;
 
-@SuppressWarnings({"MissingJavadoc", "IOResourceOpenedButNotSafelyClosed", "UtilityClass", "FeatureEnvy"})
+@SuppressWarnings({"MissingJavadoc", "UtilityClass", "FeatureEnvy"})
 public final class FullNameParser {
     public static void main(String[] args) {
         System.out.print("What is your full name? ");
-        FullName.parse(new Scanner(System.in).nextLine())
-                .ifPresentOrElse(name -> System.out.printf("""
-                                        First: %s
-                                        Middle: %s
-                                        Last: %s
-                                        Suffix: %s""",
-                                name.getFirst(),
-                                name.getMiddle().orElse("(None)"),
-                                name.getLast(),
-                                name.getSuffix().orElse("(None)")),
+        try (Scanner sc = new Scanner(System.in)) {
+            FullName.parse(sc.nextLine())
+                    .ifPresentOrElse(name -> System.out.printf("""
+                                            First: %s
+                                            Middle: %s
+                                            Last: %s
+                                            Suffix: %s""",
+                                    name.getFirst(),
+                                    name.getMiddle().orElse("(None)"),
+                                    name.getLast(),
+                                    name.getSuffix().orElse("(None)")),
 
-                        () -> System.out.println("Sorry, I don't understand."));
+                            () -> System.out.println("Sorry, I don't understand."));
+        }
     }
 }
