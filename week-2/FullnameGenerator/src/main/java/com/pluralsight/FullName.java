5073523f6c90621fbaa4df15321ffe51cb79043a
Benjamin-Bergman <git@gdane.net>
2024-04-16 11:24:22 -0400

    Add contract annotations

--- week-2/FullnameGenerator/src/main/java/com/pluralsight/FullName.java
+++ week-2/FullnameGenerator/src/main/java/com/pluralsight/FullName.java
@@ -2,13 +2,18 @@
 
 package com.pluralsight;
 
+import org.jetbrains.annotations.*;
+
 import java.util.*;
 import java.util.regex.*;
 
 @SuppressWarnings({"WeakerAccess", "StaticMethodOnlyUsedInOneClass"})
 class FullName {
     private static final Pattern PARSE_FORMAT = Pattern.compile("([^\\s,]+)\\s+(?:([^\\s,]+)\\s+)?([^\\s,]+)(?:,\\s+([^\\s,]+))?");
-    private final String firstName, lastName, middleName, nameSuffix;
+    @NotNull
+    private final String firstName, lastName;
+    @Nullable
+    private final String middleName, nameSuffix;
 
     FullName(String first, String middle, String last, String suffix) {
         firstName = clean(Objects.requireNonNull(first)).orElseThrow();
@@ -17,12 +22,15 @@ class FullName {
         nameSuffix = clean(suffix).orElse(null);
     }
 
+    @NotNull
     private static Optional<String> clean(String s) {
         return Optional.ofNullable(s)
                 .map(String::trim)
                 .flatMap(x -> x.isEmpty() ? Optional.empty() : Optional.of(x));
     }
 
+    @NotNull
+    @Contract(pure = true)
     public static Optional<FullName> parse(CharSequence fullName) {
         var matcher = PARSE_FORMAT.matcher(fullName);
         if (!matcher.find())
@@ -32,24 +40,29 @@ class FullName {
     }
 
     @SuppressWarnings("SuspiciousGetterSetter")
+    @NotNull
     public String getFirst() {
         return firstName;
     }
 
     @SuppressWarnings("SuspiciousGetterSetter")
+    @NotNull
     public String getLast() {
         return lastName;
     }
 
+    @NotNull
     public Optional<String> getMiddle() {
         return Optional.ofNullable(middleName);
     }
 
+    @NotNull
     public Optional<String> getSuffix() {
         return Optional.ofNullable(nameSuffix);
     }
 
     @Override
+    @NotNull
     public String toString() {
         StringBuilder builder = new StringBuilder();
         builder.append(firstName);
