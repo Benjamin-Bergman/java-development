a4c3d5228da78f1c40f8744c507f22223c88e70b
Benjamin-Bergman <git@gdane.net>
2024-06-10 13:15:40 -0400

    Limit data fetched

--- week-8/Mod03/src/main/java/com/pluralsight/App.java
+++ week-8/Mod03/src/main/java/com/pluralsight/App.java
@@ -6,7 +6,9 @@ package com.pluralsight;
 
 public class App {
     public static void main(String[] args) {
-        for (var row : "[.sql/] SELECT * FROM products".fetch())
-            System.out.println(row);
+        System.out.println("ID  NAME                                     PRICE   STOCK");
+        System.out.println("--- ---------------------------------------- ------- ------");
+        for (var row : "[.sql/] SELECT productId, productName, unitPrice, unitsInStock FROM products".fetch())
+            System.out.printf("%-3s %-40s %7.2f %-6d%n", row.productid, row.productname, row.unitprice, row.unitsinstock);
     }
 }
