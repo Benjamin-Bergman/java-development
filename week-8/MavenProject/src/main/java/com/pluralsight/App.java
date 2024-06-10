75d3eab7670fe11d7b562faac549089ccf20bd40
Benjamin-Bergman <git@gdane.net>
2024-06-10 09:14:02 -0400

    Add MavenProject

---
+++ week-8/MavenProject/src/main/java/com/pluralsight/App.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) Benjamin Bergman 2024.
+ */
+
+package com.pluralsight;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ week-8/MavenProject/src/test/java/com/pluralsight/AppTest.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) Benjamin Bergman 2024.
+ */
+
+package com.pluralsight;
+
+import org.junit.*;
+
+import static org.junit.Assert.*;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
